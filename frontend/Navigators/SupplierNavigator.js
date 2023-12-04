8ad38aa8c23ca3c898a12e6165dd77f7f5c55305
Aian Mark Antonio <antonioleicamarie@gmail.com>
2023-12-05 00:45:33 +0800

    Updated Supplier Navigator Source Code

--- frontend/Navigators/SupplierNavigator.js
+++ frontend/Navigators/SupplierNavigator.js
@@ -7,28 +7,13 @@ const Stack = createStackNavigator();
 import AuthGlobal from "../Context/store/AuthGlobal";
 import SupplierRegister from "../Screens/supplier/SupplierRegister";
 
-import BranchRegister from "../Screens/supplier/BranchRegister";
-import Branch from "../Screens/supplier/Branch";
+
 const SupplierNavigator = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="SupplierRegister"
         component={SupplierRegister}
-        options={{
-          headerShown: false,
-        }}
-      />
-       <Stack.Screen
-        name="Branch"
-        component={Branch}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="BranchRegister"
-        component={BranchRegister}
         options={{
           headerShown: false,
         }}
